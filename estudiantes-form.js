235746bc497b89602dd4b4e62e9749d1d77113a1
diegofa <diegoangulo1027@gmail.com>
2023-02-16 20:04:21 -0500

    Cambios de funcion eliminar

--- estudiantes-form.js
+++ estudiantes-form.js
@@ -65,5 +65,16 @@ function guardarDatos() {
 
 function eliminaRegistro(idForm){
     console.log("Presiona Eliminar");
+    document.getElementById('eliminar-btn').addEventListener('click', function() {
+        let idEliminar = parseInt(document.getElementById('id').value);
+        for (let i = 0; i < registros.length; i++) {
+            if (registros[i].id.toString() === idEliminar.toString()) {
+                registros.splice(i, 1);
+                actualizarRegistros(registros);
+                break;
+            }
+        }
+    });
 
-}
+  }
+
